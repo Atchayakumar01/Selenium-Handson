aca949eb3987f69ad3c0f48aecda13959ec25798
Atchayakumar01 <atchayakumar999@gmail.com>
2024-06-02 16:30:33 +0000

    Fluent Wait added in the code

--- src/test/java/BasicFunctions/FluentWait.java
+++ src/test/java/BasicFunctions/FluentWait.java
@@ -1,14 +1,13 @@
 package BasicFunctions;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Wait;
 
 import java.time.Duration;
+import java.util.function.Function;
 
-public class FluentWait {
+public class FluentWait<W extends SearchContext> {
     public static void main(String[] args) {
         //Fluent wait is used to  tell the Webdriver to wait for  a condition as well as the frequency with which we
         // want to check the condition before throwing an exception.
@@ -22,11 +21,21 @@ public class FluentWait {
         driver.findElement(By.id("//*[@id=\"password\"]")).sendKeys("Atchay@1999");
         driver.findElement(By.id("//*[@id=\"login\"]")).click();
 
-        Wait <WebDriver> wait = new FluentWait<WebDriver>(driver)
-                .withTimeout
-                .
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+
+        WebElement profile = wait.until(new Function<WebDriver, WebElement>() {
+
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(""));
+
+            }
 
 
+
+        });
 
 
 
